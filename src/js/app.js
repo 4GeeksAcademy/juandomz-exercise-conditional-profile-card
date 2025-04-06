de5e4c14b22135315b9b75b1f16acf7bfc18aa58
Juandomz91 <juandomz87@gmail.com>
2025-04-06 15:16:34 +0000

    jodido pero contento

--- src/js/app.js
+++ src/js/app.js
@@ -23,27 +23,51 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  console.log("These are the current variables: ", variables);
+
+  // Cover
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  // reset the website body with the new html output
+  // Safe fallback values
+  const name = variables.name || "Lucy";
+  const lastName = variables.lastName || "Boilett";
+  const role = variables.role || "Web Developer";
+  const city = variables.city || "Miami";
+  const country = variables.country || "USA";
+  const position = variables.socialMediaPosition || "position-right";
+
+  // Social Media Links
+  const socialLinks = {
+    twitter: variables.twitter
+      ? `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`
+      : "",
+    github: variables.github
+      ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`
+      : "",
+    linkedin: variables.linkedin
+      ? `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`
+      : "",
+    instagram: variables.instagram
+      ? `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`
+      : ""
+  };
+
+  // Insert dynamic content
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+                ${cover}
+              <img src="${variables.avatarURL}" class="photo" />
+              <h1>${name} ${lastName}</h1>
+              <h2>${role}</h2>
+              <h3>${city}, ${country}</h3>
+              <ul class="${position}">
+                ${socialLinks.twitter}
+                ${socialLinks.github}
+                ${socialLinks.linkedin}
+                ${socialLinks.instagram}
+              </ul>
+            </div>
+        `;
 }
 
 /**
